a6eb3ee28bf2cbd8c2ccf6dcd1526d47f70bc59e
ameraut <primalrot@gmail.com>
2015-04-16 09:12:12 -0500

    Platformer world generator

---
+++ Platformer/Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	//Author: Jonathan Moore
+	public Maze mazePrefab;
+	private Maze mazeInstance;
+	public Lava lavaPrefab;
+	private Lava lavaInstance;
+	//public Player playerPrefab;
+	//private Player playerInstance;
+
+	private void Start () {
+		BeginGame();
+	}
+	
+	private void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			RestartGame();
+		}else if(Input.GetKeyDown(KeyCode.Escape)){
+			Application.Quit();
+		}
+	}
+
+	private void BeginGame () {
+		mazeInstance = Instantiate(mazePrefab) as Maze;
+		StartCoroutine(mazeInstance.Generate());
+		lavaInstance = Instantiate (lavaPrefab) as Lava;
+		//playerInstance = Instantiate (playerPrefab) as Player;
+		//StartCoroutine (playerInstance.Generate());
+	}
+
+	private void RestartGame () {
+		StopAllCoroutines();
+		Destroy(mazeInstance.gameObject);
+		//Destroy(playerInstance.gameObject);
+		BeginGame();
+	}
+	//End Author of Jonathan Moore
+}
---
+++ Platformer/Assets/Scripts/Maze.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class Maze : MonoBehaviour {
+	//Author: Jonathan Moore
+	public int sizeX, sizeY, sizeZ;
+	public MazeCell cellPrefab;
+
+	public float generationStepDelay;
+	private MazeCell[,,] cells;
+	private int[,,] cellPositions;
+
+	public IEnumerator Generate () {
+		WaitForSeconds delay = new WaitForSeconds(generationStepDelay);
+		cells = new MazeCell[sizeX, sizeY ,sizeZ];
+		StreamReader reader = new StreamReader (@"C:\Users\User\Documents\Platformer\Assets\Scripts\platformWorld.txt");
+		string s = reader.ReadLine ();
+		int posY=0;
+		int posZ=0;
+		while(s!=null){
+			if(s!="-"){
+				for(int x = 0; x<s.Length;x++){
+					char c = s[x];
+					if(c=='1'){
+						yield return delay;
+						CreateCell(x,posY,posZ);
+					}
+				}
+				posZ++;
+			}else{
+				posY++;
+				posZ=0;
+			}
+			s = reader.ReadLine();
+		}
+	}
+
+	private void CreateCell (int x, int y,int z) {
+		MazeCell newCell = Instantiate(cellPrefab) as MazeCell;
+		cells[x, y ,z] = newCell;
+		newCell.name = "Maze Cell " + x + ", " + y + ", " + z;
+		newCell.transform.parent = transform;
+		newCell.transform.localPosition = new Vector3(x - sizeX * 0.5f + 0.5f,  y - sizeY * 0.5f + 0.5f,z - sizeZ * 0.5f + 0.5f);
+	}
+	//End of Author: Jonathan Moore
+}
