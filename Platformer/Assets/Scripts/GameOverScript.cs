a81a9125edf23ac8cf105fe1443ff5cb42bdf9ed
Carly Wilson <Carly.Jo.Wilson2011@gmail.com>
2015-04-23 21:56:25 -0500

    Menus
    So I made a start menu that has sound effects for the buttons which
    light up when mouse over. The start button (activate) should take you
    into the "GameScene" where all the action will occur. The how to play
    button (debriefe) will take you into an instructions page that tells you
    how to play and what you need to do. Both of these menus have an awesome
    background song. That I think we can use for actual game play since it's
    decent in length. The instructions page has a button that should bring
    you to the "GameScene". The game over menu has two buttons. one that
    should quit the game, and one that should take you back into the
    "GameScene" as well as a burning skeleton laughing and big a GAME OVER.
    HOWEVER! The skeleton always displays over the text for some reason. I
    will try and resolve this tomorrow. I also made a text file that we can
    list borrowed resources in.

---
+++ Platformer/Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameOverScript : MonoBehaviour {
+
+	public Texture[] frames;
+	public int framesPerSecond = 7;
+	// public Texture texture;
+
+	public Camera foregroundCamera;
+
+	public Vector2 drawPosition;
+
+	/*public void Update() {
+		// Debug.Log ("1");
+		int index = (int)(Time.time * framesPerSecond) % frames.Length;
+		// image.sprite = frames[index];
+		texture = frames[index];
+	}*/
+
+	void OnGUI()
+	{
+		GUI.depth = 0;
+		GUI.DrawTexture(new Rect(drawPosition.x, drawPosition.y, frames[0].width*4, frames[0].height*4), frames[(int)(Time.time * framesPerSecond) % frames.Length]);
+		//GUI.depth = 1;
+
+
+		if (Event.current.type == EventType.Repaint){
+			foregroundCamera.Render();
+		}
+	}
+}
