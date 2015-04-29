b9b02f81c18e44ada4810c5d3e955524d62cc53e
sleepypirate <mitchb605@gmail.com>
2015-04-28 22:25:59 -0500

    Added Character control

---
+++ Platformer/Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(CharacterController))]
+public class FirstPersonController : MonoBehaviour {
+
+	public float movementSpeed = 5.0f;
+	public float mouseSensitivity = 5.0f;
+
+	float verticalRotation = 0;
+	public float upDownRange = 60.0f;
+
+	float verticalVelocity = 0;
+
+	public float jumpSpeed = 5.0f;
+
+	CharacterController characterController;
+	// Use this for initialization
+	void Start () {
+		Screen.lockCursor = true;
+		characterController = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//rotation
+		float rotLeftRight = Input.GetAxis ("Mouse X") * mouseSensitivity;
+		transform.Rotate (0, rotLeftRight, 0);
+
+		verticalRotation -= Input.GetAxis ("Mouse Y") * mouseSensitivity;
+		verticalRotation = Mathf.Clamp(verticalRotation, -upDownRange,upDownRange);
+		Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation,0,0);
+
+		//movement
+
+		float forwardSpeed = Input.GetAxis ("Vertical") * movementSpeed;
+		float sideSpeed = Input.GetAxis ("Horizontal") * movementSpeed;
+
+		verticalVelocity += Physics.gravity.y * Time.deltaTime;
+
+		if (characterController.isGrounded && Input.GetButtonDown ("Jump")) {
+			verticalVelocity = jumpSpeed;
+		}
+
+		Vector3 speed = new Vector3 (sideSpeed, verticalVelocity, forwardSpeed);
+
+		speed = transform.rotation * speed;
+
+
+
+		characterController.Move (speed * Time.deltaTime);
+	}
+}
