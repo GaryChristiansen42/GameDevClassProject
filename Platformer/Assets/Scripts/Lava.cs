c6d45142fe7b2e2b0e99066a1a13adbf89462048
ameraut <primalrot@gmail.com>
2015-04-30 17:14:17 -0500

    Material Update
    Put in lava and lava rock textures. Altered initial placement of lava.
    Put a bottom layer of rock underneath the lava

--- Platformer/Assets/Scripts/Lava.cs
+++ Platformer/Assets/Scripts/Lava.cs
@@ -2,14 +2,9 @@
 using System.Collections;
 
 public class Lava : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
 	void Update () {
-	
+		((MovieTexture)GetComponent<Renderer>().material.mainTexture).loop = true; 
+		((MovieTexture)GetComponent<Renderer> ().material.mainTexture).Play ();
 	}
 }
